f157ccd52d623464ff0528f236ceeec60264822e
Ackincolor <lois.guillet@gmail.com>
2023-02-04 10:41:51 +0100

    création de composants

---
+++ cours1/src/components/banner/Banner.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Banner.css'
+
+function Banner() {
+    const title = "La maison jungle"
+    return (<h1 className='banner'>{title}</h1>)
+}
+
+export default Banner;
