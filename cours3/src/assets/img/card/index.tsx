34e5202f12692e0ec50261b210a94de9430059dc
Ackincolor <lois.guillet@gmail.com>
2023-02-17 19:29:12 +0100

    card viewer

---
+++ cours3/src/assets/img/card/index.tsx
@@ -0,0 +1,75 @@
+import fond1 from './1.jpeg'
+import fond2 from './2.jpeg'
+import fond3 from './3.jpeg'
+import fond4 from './4.jpeg'
+import fond5 from './5.jpeg'
+import fond6 from './6.jpeg'
+import fond7 from './7.jpeg'
+import fond8 from './8.jpeg'
+import fond9 from './9.jpeg'
+import fond10 from './10.jpeg'
+import fond11 from './11.jpeg'
+import fond12 from './12.jpeg'
+import fond13 from './13.jpeg'
+import fond14 from './14.jpeg'
+import fond15 from './15.jpeg'
+import fond16 from './16.jpeg'
+import fond17 from './17.jpeg'
+import fond18 from './18.jpeg'
+import fond19 from './19.jpeg'
+import fond20 from './20.jpeg'
+import fond21 from './21.jpeg'
+import fond22 from './22.jpeg'
+import fond23 from './23.jpeg'
+import fond24 from './24.jpeg'
+import fond25 from './25.jpeg'
+import chip from './chip.png'
+import visa from './visa.png'
+import amex from './amex.png'
+import discover from './discover.png'
+import jcb from './jcb.png'
+import troy from './troy.png'
+import unionpay from './unionpay.png'
+import dinersclub from './dinersclub.png'
+
+
+export const fond = {
+        '1' : fond1,
+        '2' : fond2,
+        '3' : fond3,
+        '4' : fond4,
+        '5' : fond5,
+        '6' : fond6,
+        '7' : fond7,
+        '8' : fond8,
+        '9' : fond9,
+        '10' : fond10,
+        '11' : fond11,
+        '12' : fond12,
+        '13' : fond13,
+        '14' : fond14,
+        '15' : fond15,
+        '16' : fond16,
+        '17' : fond17,
+        '18' : fond18,
+        '19' : fond19,
+        '20' : fond20,
+        '21' : fond21,
+        '22' : fond22,
+        '23' : fond23,
+        '24' : fond24,
+        '25' : fond25
+    };
+export const chipimage = {
+    'chip' : chip
+};
+
+export const typecard ={
+        'visa' : visa,
+        'amex' : amex,
+        'discover' : discover,
+        'jcb' : jcb,
+        'troy' : troy,
+        'unionpay' : unionpay,
+        'dinersclub' : dinersclub
+    }
