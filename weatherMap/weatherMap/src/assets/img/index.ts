c352531fdda74fce99823e55fc2131df0430484f
lois guillet <lois.guillet@gmail.com>
2023-03-08 15:52:43 +0100

    test weather

---
+++ weatherMap/weatherMap/src/assets/img/index.ts
@@ -0,0 +1,15 @@
+import clear from "./clear.png"
+import cloud from "./cloud.png"
+import mist from "./mist.png"
+import rain from "./rain.png"
+import snow from "./snow.png"
+
+const images =  {
+    "Clear":clear,
+    "Cloud": cloud,
+    "Mist": mist,
+    "Rain" :  rain,
+    "Snow" : snow
+}
+
+export default images;
